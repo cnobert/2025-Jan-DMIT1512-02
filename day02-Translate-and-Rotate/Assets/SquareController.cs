48a759501e18edf107dd02a0389e8a48496ae1eb
cnobert <cnobert@nait.ca>
2025-01-17 14:14:49 -0700

    middle of class

--- day02-Translate-and-Rotate/Assets/SquareController.cs
+++ day02-Translate-and-Rotate/Assets/SquareController.cs
@@ -4,7 +4,6 @@ public class SquareController : MonoBehaviour
 {
     public float speedX, directionX;
     public float speedY, directionY;
-
     public float rotationSpeedZ, rotationDirectionZ;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,7 +24,23 @@ public class SquareController : MonoBehaviour
         //has passed since the last call to update
         Vector2 movementAmount = new Vector2(speedX * directionX, speedY * directionY)  * Time.deltaTime;
 
+        //"transform" is the transform of the game object to which this script is attached
         transform.Translate(movementAmount, Space.World);
+
+        //the following code block from ChatGPT
+        Camera cam = Camera.main;
+        Vector3 bottomLeft = cam.ScreenToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
+        Vector3 topRight = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, cam.nearClipPlane));
+        float left = bottomLeft.x;
+        float right = topRight.x;
+        float top = topRight.y;
+        float bottom = bottomLeft.y;
+
+        if(transform.position.x >= 7.33 || transform.position.x <= -7.33)
+        {
+            directionX *= -1;
+        }
+        //Debug.Log(Time.deltaTime); TODO: look into why Time.deltaTime is 0.0009 every call to update
         
         //we it 3 "Euler" values (0-360)
         transform.Rotate(0, 0, rotationSpeedZ * rotationDirectionZ * Time.deltaTime);
