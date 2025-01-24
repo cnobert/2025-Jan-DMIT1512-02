909e6364a6c094026545fae30bfce7b2b5d32ab8
cnobert <cnobert@nait.ca>
2025-01-24 14:04:56 -0700

    break time

--- lesson03_input/Assets/ShipController.cs
+++ lesson03_input/Assets/ShipController.cs
@@ -3,18 +3,26 @@ using UnityEngine.InputSystem;
 
 public class ShipController : MonoBehaviour
 {
-    private InputAction _moveAction;
+    private InputAction _moveAction, _spinAction;
+
+    public float movementSpeed;
     void Start()
     {
         _moveAction = InputSystem.actions.FindAction("Move");
+        _spinAction = InputSystem.actions.FindAction("Spin");
     }
 
     void Update()
     {
+        #region movement
         // give the game object the ability to move: 
         //left, right, up and down 
         Vector2 moveValue = _moveAction.ReadValue<Vector2>();
-        
-        Debug.Log(moveValue);
+        moveValue *= movementSpeed * Time.deltaTime;
+        transform.Translate(moveValue);
+        #endregion
+        //give the player the ability to spin the object in either direction by pressing “j” or “k”
+        float spinDirection = _spinAction.ReadValue<float>();
+        Debug.Log(spinDirection); 
     }
 }
