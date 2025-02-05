79da241d0ce46de37e9de9924e56864289ffe015
cnobert <cnobert@nait.ca>
2025-02-05 11:40:35 -0700

    end of class

--- lesson06_prefabs/Assets/Projectile.cs
+++ lesson06_prefabs/Assets/Projectile.cs
@@ -9,4 +9,11 @@ public class Projectile : MonoBehaviour
     {
         transform.Translate(direction * speed * Time.deltaTime, Space.Self);
     }
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if(collider2D.gameObject.CompareTag("Player"))
+        {
+            Destroy(collider2D.gameObject);
+        }
+    }
 }
