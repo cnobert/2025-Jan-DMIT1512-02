bef5c5be2085b0972cdb99eabad1e23e6b012197
cnobert <cnobert@nait.ca>
2025-02-05 11:12:00 -0700

    d

--- lesson06_prefabs/Assets/AlienShip.cs
+++ lesson06_prefabs/Assets/AlienShip.cs
@@ -13,6 +13,9 @@ public class AlienShip : MonoBehaviour
         float projectileHeight = projectile.GetComponent<Renderer>().bounds.size.y;
         Vector3 newPosition = transform.position - new Vector3(0, (myHeight / 2) + (projectileHeight / 2), 0);
         projectile.transform.position = newPosition;
+
+        //in-class exercise: set the direction and speed of the projectile from here
+        
     }
 
     // Update is called once per frame
