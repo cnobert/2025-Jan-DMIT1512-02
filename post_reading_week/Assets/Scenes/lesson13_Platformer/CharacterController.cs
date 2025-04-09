6479f0aa4869826543207794371bf683b4443f10
cnobert <cnobert@nait.ca>
2025-04-09 11:27:41 -0600

    jumping

--- post_reading_week/Assets/Scenes/lesson13_Platformer/CharacterController.cs
+++ post_reading_week/Assets/Scenes/lesson13_Platformer/CharacterController.cs
@@ -4,11 +4,11 @@ using UnityEngine.InputSystem;
 public class CharacterController : MonoBehaviour
 {
     public float maxSpeed, jumpForce, moveForce;//5, 100, 200
-
+    public Transform groundCheck;
     private Rigidbody2D _myRigidBody;
     private Animator _myAnimator;
-
     private InputAction _jump, _move;
+    private bool _grounded, _jumpInitiated;
 
     void Awake()
     {
@@ -16,11 +16,18 @@ public class CharacterController : MonoBehaviour
         _myAnimator = GetComponent<Animator>();
         _jump = InputSystem.actions.FindAction("Jump");
         _move = InputSystem.actions.FindAction("Move");
+        _jumpInitiated = false;
     }
 
     void Update()
     {
-        
+        //_grounded will be true if the line passes through an object set to have the layer "Ground"
+        _grounded = 
+            Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        if(_jump.WasPressedThisFrame() && _grounded)
+        {
+            _jumpInitiated = true;
+        }    
     }
 
     void FixedUpdate()
@@ -37,6 +44,12 @@ public class CharacterController : MonoBehaviour
         if(Mathf.Abs(_myRigidBody.linearVelocityX) > maxSpeed)
         {
             _myRigidBody.linearVelocityX = Mathf.Sign(_myRigidBody.linearVelocityX) * maxSpeed;
+        }
+        if(_jumpInitiated)
+        {
+            _myRigidBody.AddForce(new Vector2(0, jumpForce));
+            _myAnimator.SetTrigger("Jump");
+            _jumpInitiated = false;
         }
     }
 }
